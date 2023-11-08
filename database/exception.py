ebc47bcd23ccbb523404388bd295b6eeea9a20f7
PKPY <p01001011p01011001@gmail.com>
2023-11-08 19:43:49 +0330

    add more exceptions

--- database/exception.py
+++ database/exception.py
@@ -1,3 +1,8 @@
-def already_exist(user_input):
-    return f'{user_input} is already exist! :('
+class Exception(Exception):
+    
+    def AlreadyExist(user_input):
+        return f'{user_input} is already exist! :('
 
+    def InvalidPassword():
+        return 'password is invalid! :('
+    
